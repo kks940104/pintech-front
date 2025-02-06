99de90499474995385beaad071bffc839c3d81d3
kks940104 <kks940104@gmail.com>
2025-02-06 12:59:54 +0900

    helmet 추가 완

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
 import StyledComponentsRegistry from './registry'
 import Header from './global/ui/outlines/Header'
 import Footer from './global/ui/outlines/Footer'
+import { CommonProvider } from './global/contexts/CommonContext'
+import { Metadata } from 'next'
+
 import './globals.css'
+
+export const metadata: Metadata = {
+  title: '핀테크 과정 포트폴리오',
+  description: '설명..',
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,9 +20,11 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <StyledComponentsRegistry>
-          <Header />
-          <main className="main-centent">{children}</main>
-          <Footer />
+          <CommonProvider>
+            <Header />
+            <main className="main-centent">{children}</main>
+            <Footer />
+          </CommonProvider>
         </StyledComponentsRegistry>
       </body>
     </html>
